100f66d346fc362ba88ec32d94f6322a560071e0
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2022-05-09 18:57:24 +0300

    hold the dice (first part)

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Confetti from "react-confetti"
 import Die from "./Die"
+import {nanoid} from "nanoid"
 
 export default function App() {
   
@@ -9,17 +10,34 @@ export default function App() {
   function allNewDice(){
     const arrayNumbers = []
     for(var i=0; i<10;i++){
-      arrayNumbers.push(Math.floor(Math.random() * 6)+1)
+      arrayNumbers.push({
+        value: Math.ceil(Math.random() * 6), 
+        isHeld: false,
+        id:nanoid()
+      })
     }
     return arrayNumbers
   }
+
+  function changeDice(){
+    setDice(allNewDice())
+  }
    
+  function holdDice(id){
+    //looping to all the dice, if the id matches reverse it else keep it
+    setDice(prev => prev.map(die => {
+        return die.id === id ? 
+      {...die, isHeld: !die.isHeld} :die
+    }))
+  }
+
 const diceItem = dice.map(die => 
-  <Die value = {die}/> )
+  <Die key={die.id} onClick={holdDice} value = {die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/> )
 
    return(  
    <main> 
        {diceItem}
+       <button className="roll-dice" onClick={changeDice}>Change</button>
   </main>
   )
   
--- tenzies/src/Die.js
+++ tenzies/src/Die.js
@@ -1,8 +1,11 @@
 import React from "react"
 
 export default function Die(props) {
+    const styles = {
+        backgroundColor:props.isHeld === true ? "#59E391" : "#ffffff"
+      }
    return(
-       <div className="die-face">
+       <div className="die-face" style={styles} onClick={props.holdDice}>
             <h2 className="die-num">
                 {props.value}
             </h2>
