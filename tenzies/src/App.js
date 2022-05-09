3d0b9e8cab7fdb6c2546042354b1076f4261be81
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2022-05-09 14:37:02 +0300

    dice added
    Dice added using map over an array with 10 random values from 1-6

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -3,6 +3,24 @@ import Confetti from "react-confetti"
 import Die from "./Die"
 
 export default function App() {
+  
+  const [dice,setDice] = React.useState(allNewDice())
+
+  function allNewDice(){
+    const arrayNumbers = []
+    for(var i=0; i<10;i++){
+      arrayNumbers.push(Math.floor(Math.random() * 6)+1)
+    }
+    return arrayNumbers
+  }
    
-    
+const diceItem = dice.map(die => 
+  <Die value = {die}/> )
+
+   return(  
+   <main> 
+       {diceItem}
+  </main>
+  )
+  
 }
--- tenzies/src/Die.js
+++ tenzies/src/Die.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Die(props) {
-   
+   return(
+       <div className="die-face">
+            <h2 className="die-num">
+                {props.value}
+            </h2>
+       </div>
+   )
 }
