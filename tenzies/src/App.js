6050174f38000d3af36ded9cdc74589b0c48947e
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2022-05-10 14:04:28 +0300

    Custom timer added

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -3,18 +3,38 @@ import Confetti from "react-confetti"
 import Die from "./Die"
 import {nanoid} from "nanoid"
 
+
 export default function App() {
   
   const [dice,setDice] = React.useState(allNewDice())
   const [tenzies, setTenzies] = React.useState(false)
+  const [seconds,setSeconds] = React.useState(0)
+  const [minutes,setMinutes] = React.useState(0)
+
+  var timer;
+ 
+    React.useEffect(()=>{
+      if(!tenzies){
+      timer = setInterval(()=>{
+        setSeconds(seconds+1)
+        if(seconds==59){
+          setMinutes(minutes+1)
+          setSeconds(0)
+        }
+    },1000)
+      return () => clearInterval(timer)
+    }})
+  
+ 
 
   React.useEffect(()=>{
-   
    const allHeld = dice.every(die => die.isHeld) //true or false, depends if all of dice are true
    const firstValue = dice[0].value
    const equalVal = dice.every(die => die.value === firstValue)
     if(allHeld && equalVal){
       setTenzies(true)
+      setMinutes(0)
+      setSeconds(0)
       console.log("you won!!")
     }
 
@@ -63,7 +83,10 @@ const diceItem = dice.map(die =>
    return(  
    <main> 
        {diceItem}
+       <div>
+       <div><h2>Your time:{minutes} minutes and {seconds} seconds</h2></div>
        <button className="roll-dice" onClick={changeDice}>{ tenzies ? `New Game` : `Change` }</button>
+       </div>
        {tenzies && <Confetti />}
   </main>
   )
